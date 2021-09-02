689eef3c150642c42d21229e6ca1ab4d38b208b3
Axel_SECHET <axel.sechet@gmail.com>
2021-09-02 17:05:36 +0200

    Git Spring Initializr

---
+++ demowebmangalist/src/test/java/com/webmangalist/demowebmangalist/DemowebmangalistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmangalist.demowebmangalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebmangalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
