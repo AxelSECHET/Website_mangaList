4cabbae2fea43b4bbeebd430d0c9a23aa36dc151
Axel_SECHET <axel.sechet@gmail.com>
2021-09-10 11:40:34 +0200

    Get info manga via input

--- demowebmangalist/src/main/java/com/webmangalist/demowebmangalist/controllers/HomeController.java
+++ demowebmangalist/src/main/java/com/webmangalist/demowebmangalist/controllers/HomeController.java
@@ -9,6 +9,7 @@ import java.net.http.HttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -17,33 +18,59 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController{
 
     @GetMapping("/")
-    public String index(ModelMap modelMap){
+    public String index(@RequestParam(required = false) String manga, ModelMap modelMap){
         String[] friends = {"John","Alice","Bob"};
         modelMap.addAttribute("friends", friends);
-        String infoAPI = getAPI().strip();
+        
+        modelMap.addAttribute("manga", manga);
+        String infoAPI = getAPI(manga);
         modelMap.addAttribute("infoAPI", infoAPI);
         return "home";
     }
 
-    public String getAPI(){
+    public String getAPI(String manga){
         try {
+            manga = manga.replace(" ", "+");
+            System.out.println(manga);
             /*HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://jikan1.p.rapidapi.com/manga/2/characters"))
                 .header("x-rapidapi-host", "jikan1.p.rapidapi.com")
                 .header("x-rapidapi-key", "a2bf8adc01mshdd0dca505b69705p136aebjsna4edc5215179")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
-
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println("oui API " + response.body());*/
-            HttpRequest request = HttpRequest.newBuilder()
+
+            /*HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://animanga2.p.rapidapi.com/manga/search/solo%20leveling"))
                 .header("x-rapidapi-host", "animanga2.p.rapidapi.com")
                 .header("x-rapidapi-key", "a2bf8adc01mshdd0dca505b69705p136aebjsna4edc5215179")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());*/
+
+            /*HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://kitsu.io/api/edge/manga?filter%5Btext%5D=attack+on+titan"))
+                .header("Accept", "application/vnd.api+json")
+                .header("Content-Type", "application/vnd.api+json")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());*/
+
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://kitsu.io/api/edge/manga?filter%5Btext%5D="+ manga))
+                .header("Accept", "application/vnd.api+json")
+                .header("Content-Type", "application/vnd.api+json")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println(response.body());
+
+
+
+
             return response.body();
         } catch (Exception e) {
             System.out.println("error API");
